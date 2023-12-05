685065b028e1d8aefb75bfb5efbc70ee2de3e177
Daniel de Almeida <dani1114vivilinda@gmail.com>
2023-12-05 17:50:28 -0300

    Primeiro commit

---
+++ demo/src/test/java/com/example/demo/MessagingStompWebsocketApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.example.messagingstompwebsocket.WebSocketConfig;
+
+@SpringBootTest(classes = WebSocketConfig.class)
+class MessagingStompWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ webrestfultutorial/src/test/java/com/example/webrestfultutorial/ConsumingRestApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.webrestfultutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.consumingrest.ConsumingRestApplication;
+
+@SpringBootTest(classes = ConsumingRestApplication.class)
+class ConsumingRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
